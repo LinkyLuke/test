cc52a4015c228b15b46e5ff9bde40f58d2ebd7a9
Luke <luke@coltman.me>
2018-02-18 11:57:11 +0000

    First branch

--- Test.py
+++ Test.py
@@ -1,9 +1,15 @@
 print('What is your name')
 name = input()
-if name.upper() == 'LUKE':
-	print('Hi Luke')
-elif name.upper() == 'DAN':
-	print('Hi Dan')
+if name.upper() == 'LUKE' or 'DAN':
+	print('Hi {}'.format(name))
+	print('What is your age')
+	age = input()
+	if age == 24:
+		print('Hah you old as fuck')
+	elif age == 23:
+		print('Decent, decent')
+	else: 
+		print('YOU LIE')
 else:
 	print('Fuck off')
 input()
